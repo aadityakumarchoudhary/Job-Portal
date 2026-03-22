008b903ab10f9a9cc161e183e30db20b131859f8
Arya kuraria <aryakuraria@Aryas-MacBook-Air.local>
2026-03-22 11:45:41 +0530

    first commit

---
+++ backend/routes/user.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { login, logout, register, updateProfile } from "../controllers/user.controllers.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
+
+const router = express.Router();
+
+//router.route("/register").post(register);
+router.post("/register",register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+
+export default router;
